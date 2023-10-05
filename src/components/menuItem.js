521184fe3f7175e8bdae625cf10339ea2e32f243
miguel <91511269+ghmacg@users.noreply.github.com>
2023-10-05 18:53:51 -0400

    Create module to create DOM elements of menu items

---
+++ src/components/menuItem.js
@@ -0,0 +1,25 @@
+const menuItem = (name, price, description='') => {
+    const container = document.createElement('div');
+    container.classList.add('item-container');
+
+    const itemName = document.createElement('div');
+    itemName.textContent = name;
+    itemName.classList.add('item-name');
+
+    const itemPrice = document.createElement('div');
+    itemPrice.textContent = price;
+    itemPrice.classList.add('item-price');
+    
+    container.appendChild(itemName);
+
+    if (description) {
+        const itemDescription = document.createElement('div');
+        itemDescription.textContent = description
+        itemDescription.classList.add('item-description');
+        container.appendChild(itemDescription);
+    };
+
+    container.appendChild(itemPrice);
+}
+
+export default menuItem;
