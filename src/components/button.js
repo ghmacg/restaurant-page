fc9f0a9f8a5852fda54bdb7b7ff2a2e047c6799e
miguel <91511269+ghmacg@users.noreply.github.com>
2023-10-06 18:40:57 -0400

    Refactor code to use it with multiple pages

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,7 @@
-const createBtn = (text, cssClass='') => {
+const createBtn = (text, cssClass='', type='') => {
     const button = document.createElement('button');
     if (cssClass) button.classList.add(cssClass);
+    if (type) button.setAttribute('type', type);
     button.textContent = text;
     return button;
 }
