9e676681e775eec6ea484f417180fda1b0014c07
miguel <91511269+ghmacg@users.noreply.github.com>
2023-10-05 20:36:54 -0400

    Create module for header of the page

---
+++ src/pages/Header/header.js
@@ -0,0 +1,23 @@
+import createBtn from '../../components/button';
+
+const header = () => {
+    const header = document.createElement('div');
+    header.id = 'header';
+    
+    const title = document.createElement('div');
+    title.classList.add('title');
+    title.textContent = 'BEEF';
+
+    const navbar = document.createElement('div');
+    navbar.classList.add('nav-bar');
+    navbar.appendChild(createBtn('Home', 'home-button'));
+    navbar.appendChild(createBtn('Menu', 'menu-button'));
+    navbar.appendChild(createBtn('Contact', 'contact-button'));
+
+    header.appendChild(title);
+    header.appendChild(navbar);
+    
+    return header;
+}
+
+export default header;
