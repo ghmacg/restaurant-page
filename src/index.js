552a000dedf8dc29af8417022af7c9896fc51c44
miguel <91511269+ghmacg@users.noreply.github.com>
2023-10-05 20:36:05 -0400

    Create main module for page

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import firstLoad from "./functions/firstLoad";
+import home from "./pages/Home/home";
+import menu from "./pages/Menu/menu";
+import contact from "./pages/Contact/contact";
+
+firstLoad();
+
+const homeBtn = document.querySelector('.home-button');
+const menuBtn = document.querySelector('.menu-button');
+const contactBtn = document.querySelector('.contact-button');
+const main = document.getElementById('main');
+
+homeBtn.addEventListener('click', () => {
+    main.innerText = '';
+    main.appendChild(home());
+});
+
+menuBtn.addEventListener('click', () => {
+    main.innerText = '';
+    main.appendChild(menu());
+});
+
+contactBtn.addEventListener('click', () => {
+    main.innerText = '';
+    main.appendChild(contact());
+});
