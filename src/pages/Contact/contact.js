e226a8abf3c9ff05992ae676cb5ddbd98bd3eb5b
miguel <91511269+ghmacg@users.noreply.github.com>
2023-10-07 13:01:46 -0400

    Add comments to visualize the structure on contact page

--- src/pages/Contact/contact.js
+++ src/pages/Contact/contact.js
@@ -5,6 +5,18 @@ import createBtn from '../../components/button';
 import createMap from '../../components/map';
 
 const contact = () => {
+    /* Page structure
+    **
+    ** contact > 
+    **    div #title
+    **    contactContainer >
+    **        infoContainer >
+    **            div #contact-info * 3
+    **            formContainer >
+    **                div #form-title
+    **                form
+    **        map          
+    */
     const contact = document.createElement('div');
     contact.id = 'contact';
     
@@ -25,17 +37,33 @@ const contact = () => {
     form.appendChild(createTextArea('TYPE YOUR MESSAGE...', 'message'));
     form.appendChild(createBtn('Send', 'form-button', 'submit'));
     
+    /*            formContainer >
+    **                div #form-title
+    **                form
+    */
     formContainer.appendChild(createDiv('Message Us', 'form-title'));
     formContainer.appendChild(form);
 
+    /*        infoContainer >
+    **            div #contact-info * 3
+    **            formContainer...
+    */
     infoContainer.appendChild(createDiv('666 N Orleans St, Chicago, IL 60654', 'contact-info'));
     infoContainer.appendChild(createDiv('Mon - Sat	10:00 AM - 3:30 PM', 'contact-info'));
     infoContainer.appendChild(createDiv('(312) 337-8500', 'contact-info'));
     infoContainer.appendChild(formContainer);
 
+    /*    contactContainer >
+    **        infoContainer...
+    **        map
+    */
     contactContainer.appendChild(infoContainer);
     contactContainer.appendChild(createMap());
     
+    /* contact > 
+    **    div #title
+    **    contactContainer...
+    */
     contact.appendChild(createDiv('Contact Us', 'contact-title'));
     contact.appendChild(contactContainer);
     return contact;
