b7eca87e2a339c0f6669c9eb009d09cac8ee5c4c
miguel <91511269+ghmacg@users.noreply.github.com>
2023-10-07 11:13:52 -0400

    Import styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import home from "./pages/Home/home";
 import menu from "./pages/Menu/menu";
 import contact from "./pages/Contact/contact";
 import "normalize.css";
+import "./style.css";
+import "./pages/Header/style.css";
+import "./pages/Home/style.css";
+import "./pages/Menu/style.css";
+import "./pages/Contact/style.css";
 
 firstLoad();
 
