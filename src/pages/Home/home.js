df6acc39d00fcdc59ec6a13751875db2cfa8693e
miguel <91511269+ghmacg@users.noreply.github.com>
2023-10-09 14:05:34 -0400

    Add home elements

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,14 +1,17 @@
-import createImg from "../../components/img";
-import createParagraph from "../../components/paragraph";
-import chefImg from "../../assets/img/chef.jpg";
+import createBtn from "../../components/button";
+import createDiv from "../../components/div";
 
 const home = () => {
 	const home = document.createElement('div');
 	home.id = 'home';
 
-	home.appendChild(createParagraph('Local family sandwich shop'));
-	home.appendChild(createParagraph('Managed by aclaimed chef Carmy Berzatto'))
-	home.appendChild(createImg(chefImg, 'Chef'));
+	const textContainer = document.createElement('div');
+	textContainer.id = 'text-container';
+	textContainer.appendChild(createDiv("Chicago’s beloved Italian sandwich shop!"));
+	textContainer.appendChild(createDiv('Serving since 1997'));
+
+	home.appendChild(textContainer);
+	home.appendChild(createBtn('View Menu', 'view-menu-button'));
   
 	return home;
 }
